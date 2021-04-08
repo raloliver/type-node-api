491b99c72bbc8098d18b8c288e9af4b74dea579b
raloliver <contato@raloliver.com.br>
2021-04-08 14:52:57 +0100

    app.ts

---
+++ src/app.ts
@@ -0,0 +1,35 @@
+import express, {Application} from 'express';
+
+const PORT = 3000;
+
+class App {
+  public app: Application;
+
+  constructor(appConfig: {port: number; middlewares: any; controllers: any}) {
+    this.app = express();
+    this.setMiddlewares(appConfig.middlewares);
+    this.setControllers(appConfig.controllers);
+  }
+
+  public listen() {
+    this.app.listen(PORT);
+  }
+
+  private setMiddlewares(middlewares: {
+    forEach: (mid: (middlewares: any) => void) => void;
+  }) {
+    middlewares.forEach((middleware) => {
+      this.app.use(middleware);
+    });
+  }
+
+  private setControllers(controllers: {
+    forEach: (control: (controllers: any) => void) => void;
+  }) {
+    controllers.forEach((controller) => {
+      this.app.use('/', controller);
+    });
+  }
+}
+
+export default App;
